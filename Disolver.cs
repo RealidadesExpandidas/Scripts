917060efebd9bcef867b5edfced554e0686d58f8
baronrampante <baronrampante@users.noreply.github.com>
2021-11-10 13:20:39 -0300

    Add files via upload
    con el movimiento x del controlador

--- Disolver.cs
+++ Disolver.cs
@@ -7,14 +7,17 @@ public class Disolver : MonoBehaviour
     
     
     public GameObject dis;// Start is called before the first frame update
+    public Transform mano;
     void Start()
     {
-        dis.GetComponent<Renderer>().material.SetFloat("Dissolve", .5f);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float ManoX = mano.GetComponent<Transform>().localPosition.x;
+        dis.GetComponent<Renderer>().material.SetFloat("Dissolve", ManoX);
+        print(ManoX);
     }
 }
